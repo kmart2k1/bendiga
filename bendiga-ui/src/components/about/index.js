d742ec8e83d7cae92d28921bf0cd5ccc2fa3ba19
keith-mar <keith.mar@govx.com>
2022-10-15 00:06:19 -0700

    uat feedback

--- bendiga-ui/src/components/about/index.js
+++ bendiga-ui/src/components/about/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { createUseStyles, useTheme } from 'react-jss'
 import Grid from '@mui/material/Grid'
-import jakeImg from '../../assets/me.png'
+import jakeImg from '../../assets/about/jakephoto.jpg'
 import beeArt from '../../assets/bee-art.png'
 import classNames from 'classnames'
 import sideart from '../../assets/about/sideart.png'
 import topart from '../../assets/about/topart.png'
 import logo from '../../assets/about/logo.png'
+import butterfly from '../../assets/about/butterfly.png'
 const useStyles = createUseStyles((theme) => ({
   container: {
     backgroundColor: 'rgba(194,202,134,.2)',
     fontFamily: 'Montserrat, sans-serif',
+    color: '#666666',
   },
   topPart: {
     backgroundImage: `url(${beeArt})`,
@@ -28,15 +30,15 @@ const useStyles = createUseStyles((theme) => ({
     flexDirection: 'column',
   },
   subheader: {
-    borderBottom: 'solid 1px black',
-    fontSize: '18px',
+    fontSize: '24px',
+    color: '#c2ca86',
     fontWeight: 'bold',
     fontStyle: 'italic',
   },
   jake: {
-    padding: '8px 8px 0 0',
-    width: '400px',
-    height: '500px',
+    padding: '12px 0 0 0',
+    width: '180px',
+    height: '225px',
   },
   jakeText: {
     padding: '8px',
@@ -55,18 +57,18 @@ const useStyles = createUseStyles((theme) => ({
     display: 'flex',
     backgroundColor: '#c2ca86',
     alignItems: 'center',
-    height: '60px',
+    height: '80px',
   },
   navItem: {
     fontFamily: 'Montserrat, sans-serif',
     color: 'white',
     fontStyle: 'italic',
-    fontSize: '20px',
+    fontSize: '28px',
     margin: '0 20px',
   },
   paper: {
-    maxWidth: '900px',
-    padding: '0px 96px 48px 96px',
+    maxWidth: '1200px',
+    padding: '0px 48px 48px 48px',
     margin: '0 auto',
     backgroundColor: 'white',
     textAlign: 'left',
@@ -75,6 +77,11 @@ const useStyles = createUseStyles((theme) => ({
   logo: {
     position: 'absolute',
     height: '50px',
+  },
+  butterfly: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
   },
   topPlant: {
     width: '400px',
@@ -92,10 +99,10 @@ const AboutPage = () => {
   return (
     <>
       <div className={classes.logo}>
-        <img src={logo} style={{ height: '60px' }} />
+        <img src={logo} style={{ height: '80px' }} />
       </div>
       <div className={classes.navContainer}>
-        <div className={classes.navItem} style={{ marginLeft: '80px' }}>
+        <div className={classes.navItem} style={{ marginLeft: '100px' }}>
           home
         </div>
         <div className={classes.navItem}>about </div>
@@ -103,75 +110,97 @@ const AboutPage = () => {
         <div className={classes.navItem}>work</div>
         <div className={classes.navItem}>contact</div>
       </div>
+      <div className={classes.butterfly}>
+        <a href="reading">
+          <img src={butterfly} style={{ height: '40px' }} />
+        </a>
+      </div>
       <div className={classes.container}>
         <div className={classes.paper}>
-          <div className={classes.topArtContainer}>
-            <img src={topart} className={classes.topPlant} />
-          </div>
           <Grid container>
-            <Grid item xs={12} className={classes.subheader}>
-              bendiga
-            </Grid>
-            <Grid item xs={12}>
-              <p>
-                Bendiga provides landscape architectural design to the San
-                Francisco Bay Area. The company focuses on connecting people to
-                nature through artful, practical and sustainable design.
-              </p>
-              <p>
-                Sustainability calls for using plants suited for California,
-                irrigating efficiently, reducing stormwater runoff, and
-                respecting the site. California's natural resources are a
-                blessing, and we believe in using them responsibly.
-              </p>
-              <p>
-                Taking care of the environment and meeting the needs of people
-                and their budget is a creative challenge. By bringing in the
-                beauty of our surroundings, dynamic spaces full of life can be
-                found by family and creature alike.
-              </p>
-              <p>Spaces that we can call Home.</p>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              className={classes.subheader}
-              style={{ marginTop: '20px' }}
-            >
-              jacob mar
-            </Grid>
-            <Grid item>
-              <div style={{ display: 'flex' }}>
-                <img src={jakeImg} className={classes.jake} />
-                <div style={{ paddingLeft: '24px' }}>
+            <Grid item xs={8}>
+              <Grid container>
+                <Grid
+                  item
+                  xs={12}
+                  className={classes.subheader}
+                  style={{ marginTop: '36px' }}
+                >
+                  bendiga
+                </Grid>
+                <Grid item xs={12}>
                   <p>
-                    A career in landscaping began on a cul-de-sac in Almaden
-                    Valley. Jake's parents and neighbors gave him summer jobs
-                    taking care of their gardens. Plants, even pulling weeds,
-                    always seemed to give Jake a certain calmness.
+                    Bendiga provides landscape architectural design to the San
+                    Francisco Bay Area. The company focuses on connecting people
+                    to nature through artful, practical and sustainable design.
                   </p>
+                  <p>
+                    Sustainability calls for using plants suited for California,
+                    irrigating efficiently, reducing stormwater runoff, and
+                    respecting the site. California's natural resources are a
+                    blessing, and we believe in using them responsibly.
+                  </p>
+                  <p>
+                    Taking care of the environment and meeting the needs of
+                    people and their budget is a creative challenge. By bringing
+                    in the beauty of our surroundings, dynamic spaces full of
+                    life can be found by family and creature alike.
+                  </p>
+                  <p>Spaces that we can call Home.</p>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  className={classes.subheader}
+                  style={{ marginTop: '20px' }}
+                >
+                  jacob mar
+                </Grid>
+                <Grid item>
+                  <Grid container>
+                    <Grid item xs={4}>
+                      <div style={{ padding: '16px 24px 0 0' }}>
+                        <img
+                          src={jakeImg}
+                          style={{
+                            width: '100%',
+                            height: 'auto',
+                          }}
+                        />
+                      </div>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <p>
+                        A career in landscaping began on a cul-de-sac in Almaden
+                        Valley. Jake's parents and neighbors gave him summer
+                        jobs taking care of their gardens. Plants, even pulling
+                        weeds, always seemed to give Jake a certain calmness.
+                      </p>
 
-                  <p>
-                    Since then, Jake has graduated with a landscape architecture
-                    degree from Cal Poly, SLO, got his license, and worked on
-                    parks, trail, and residential projects at design firms
-                    around the bay for the past 16 years. He also helped build
-                    gardens and worked as a fine gardener, learning valuable
-                    skills from properly swinging a pick to creating habitat for
-                    the Monarch butterfly.
-                  </p>
-                  <img className={classes.flowerImage} src={sideart} />
-                  <p>
-                    Jake is grateful for these experiences and all the people he
-                    crossed paths with. They showed him the beauty of God's
-                    creation and the peace of knowing Jesus Christ
-                  </p>
-                </div>
-              </div>
+                      <p>
+                        Since then, Jake has graduated with a landscape
+                        architecture degree from Cal Poly, SLO, got his license,
+                        and worked on parks, trail, and residential projects at
+                        design firms around the bay for the past 16 years. He
+                        also helped build gardens and worked as a fine gardener,
+                        learning valuable skills from properly swinging a pick
+                        to creating habitat for the Monarch butterfly.
+                      </p>
+                      <p>
+                        Jake is grateful for these experiences and all the
+                        people he crossed paths with. They showed him the beauty
+                        of God's creation and the peace of knowing Jesus Christ
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <img src={topart} className={classes.topPlant} />
             </Grid>
           </Grid>
+          <Grid container></Grid>
         </div>
       </div>
     </>
